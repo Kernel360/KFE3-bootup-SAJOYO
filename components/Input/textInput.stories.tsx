c262a236649a166fb77bb661e6b1fa925c0c14dc
Hyejin <hyejin.frontend@gmail.com>
2025-06-06 23:19:20 +0900

    chore: 스토리북 title경로 변경

--- components/Input/textInput.stories.tsx
+++ components/Input/textInput.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { TextInput } from './textInput';
 
 const meta: Meta<typeof TextInput> = {
-  title: 'Example/TextInput',
+  title: 'components/TextInput',
   component: TextInput,
   parameters: {
     layout: 'centered',
@@ -10,13 +10,12 @@ const meta: Meta<typeof TextInput> = {
   tags: ['autodocs'],
   args: {
     label: '',
-    placeholder: '내용을 입력해주세요', 
-  }
+    placeholder: '내용을 입력해주세요',
+  },
 };
 
 export default meta;
 type Story = StoryObj<typeof TextInput>;
-
 
 export const Default: Story = {};
 
@@ -27,16 +26,26 @@ export const Typing: Story = {
 };
 
 export const WithIcon: Story = {
-    args: {
-        icon: <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M480-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160-160v-112q0-34 17.5-62.5T224-378q62-31 126-46.5T480-440q66 0 130 15.5T736-378q29 15 46.5 43.5T800-272v112H160Z"/></svg>,
-        placeholder: '아이디를 입력해주세요.'
-    },
+  args: {
+    icon: (
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        height='24px'
+        viewBox='0 -960 960 960'
+        width='24px'
+        fill='#000000'
+      >
+        <path d='M480-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160-160v-112q0-34 17.5-62.5T224-378q62-31 126-46.5T480-440q66 0 130 15.5T736-378q29 15 46.5 43.5T800-272v112H160Z' />
+      </svg>
+    ),
+    placeholder: '아이디를 입력해주세요.',
+  },
 };
 export const WithLabel: Story = {
-    args: {
-        label: '받으실 분',
-        required: true,
-    },
+  args: {
+    label: '받으실 분',
+    required: true,
+  },
 };
 
 export const Error: Story = {
